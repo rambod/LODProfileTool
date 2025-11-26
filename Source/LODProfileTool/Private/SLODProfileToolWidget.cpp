d5654ef9f809cdff2a56d417777b54c77fe37f1c
Rambod Ghashghai <gh.rambod@gmail.com>
2025-11-26 13:05:56 +0330

    Add FilterPlugin.ini and include Content Browser headers
    Added a new FilterPlugin.ini configuration file with packaging instructions. Also included ContentBrowserModule and IContentBrowserSingleton headers in SLODProfileToolWidget.cpp to support content browser functionality.

--- Source/LODProfileTool/Private/SLODProfileToolWidget.cpp
+++ Source/LODProfileTool/Private/SLODProfileToolWidget.cpp
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+#include "ContentBrowserModule.h"
+#include "IContentBrowserSingleton.h"
 #include "SLODProfileToolWidget.h"
 
 #include "ContentBrowserModule.h"
